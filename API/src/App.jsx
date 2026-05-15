95afe9409c740e5557420390ef77a9191910dbb7
unknown <behruzn081@gmail.com>
2026-05-15 15:33:27 +0300

    15.05.2026 15:33

---
+++ API/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+import './App.css'
+import MovieSearch from './MovieSearch'
+
+function App() {
+
+  return (
+    <div>
+    <MovieSearch/>
+    </div>
+  )
+}
+
+export default App
